c3b2ee83db29373a7301296b662f3bce48d49483
Thinkman Wang <wangxf1985@gmail.com>
2022-04-13 19:22:13 +0800

    remove useless service

--- worldservice/WorldService.go
+++ worldservice/WorldService.go
@@ -2,21 +2,27 @@ package worldservice
 
 import (
 	"ThinkGOChat/myprotocol"
-	"ThinkGOChat/thinkutils"
 	"fmt"
 	"github.com/lonng/nano"
 	"github.com/lonng/nano/component"
 	"github.com/lonng/nano/session"
 )
 
+type User struct {
+	Session *session.Session
+}
+
 type WorldService struct {
 	component.Base
 	group *nano.Group
+
+	Users map[string]*User
 }
 
 func newWorldService() *WorldService {
 	return &WorldService{
 		group: nano.NewGroup("all-users"),
+		Users: map[string]*User{},
 	}
 }
 
@@ -30,24 +36,12 @@ func (this *WorldService) SendMessage(s *session.Session, msg *SendMessage) erro
 	return this.group.Broadcast("onMessage", msg)
 }
 
-func (this *WorldService) userDisconnected(s *session.Session) {
-	if err := this.group.Leave(s); err != nil {
-		log.Info("Remove user from group failed", s.UID(), err)
-		return
-	}
-	log.Info("User session disconnected", s.UID())
-}
-
-func (this *WorldService) NewUser(s *session.Session, msg *myprotocol.NewUserRequest) error {
-	log.Info("%d", s.ID())
-	log.Info(thinkutils.JSONUtils.ToJson(msg))
-
-	return nil
-}
-
 func (this *WorldService) OnConnected(s *session.Session, msg *myprotocol.NewUserRequest) error {
 	log.Info("%p %s JoinRoom", s, msg.OpenId)
 	s.Set("openId", msg.OpenId)
+	this.Users[msg.OpenId] = &User{
+		Session: s,
+	}
 
 	broadcast := &myprotocol.NewUserBroadcast{
 		Content: fmt.Sprintf("User user join: %v", msg.Nickname),
@@ -56,12 +50,14 @@ func (this *WorldService) OnConnected(s *session.Session, msg *myprotocol.NewUse
 	}
 
 	if err := this.group.Add(s); err != nil {
-
 	}
+
+	log.Info("All User Count: %d", len(this.Users))
 
 	return nil
 }
 
 func (this *WorldService) OnDisconnected(s *session.Session) {
-
+	delete(this.Users, s.String("openId"))
+	log.Info("All User Count: %d", len(this.Users))
 }
